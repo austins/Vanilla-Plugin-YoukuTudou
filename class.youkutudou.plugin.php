3dbe1266c4dc664ab052df5ff5e3df77f6b71828
Shadowdare <shadowdare@users.noreply.github.com>
2015-02-01 19:28:59 -0800

    Increment to 1.4.2

--- class.youkutudou.plugin.php
+++ class.youkutudou.plugin.php
@@ -22,7 +22,7 @@
 $PluginInfo['YoukuTudou'] = array(
    'Name' => 'Youku & Tudou Embed',
    'Description' => '允许在主题、回复和动态中嵌入优酷网、土豆网和AcFun的视频。 Embed Youku, Tudou, and AcFun videos in posts.',
-   'Version' => '1.4.1',
+   'Version' => '1.4.2',
    'Author' => "Shadowdare",
    'AuthorUrl' => 'http://vanillaforums.org/profile/addons/16014/Shadowdare',
    'License' => 'GNU GPL2',
